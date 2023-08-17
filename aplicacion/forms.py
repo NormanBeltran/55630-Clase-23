cc8ef0146ddd966c11227384f3523367654d75ea
NormanBeltran <josenormanbeltran@gmail.com>
2023-08-17 20:46:59 -0300

    Clase 23

---
+++ aplicacion/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CursoForm(forms.Form):
+    nombre = forms.CharField(max_length=50, required=True)
+    comision = forms.IntegerField(required=True)
+    email = forms.EmailField(label="Email", required=False)
+    TURNOS = (
+        (1, "Mañana"),
+        (2, "Tarde"),
+        (3, "Noche"),
+    )
+    turno = forms.ChoiceField(label="Turno elegido", choices=TURNOS, required=True)
+    becado = forms.BooleanField()
+
+class ProfesorForm(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=50, required=True)
+    apellido = forms.CharField(label="Apellido", max_length=50, required=True)
+    email = forms.EmailField(required=True)
+    profesion = forms.CharField(label="Actividad", max_length=50, required=True)
+
+class RegistroUsuariosForm(UserCreationForm):      
+    email = forms.EmailField(label="Email de Usuario")
+    password1 = forms.CharField(label="Contraseña", widget= forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmar Contraseña", widget= forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        
